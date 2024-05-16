a62a60f77761dd716a58d174c8698c288341d98a
이민서 <lms316497@naver.com>
2024-05-16 11:01:33 +0900

    전체 서브메뉴 떨어지게 완성

--- 02-GNB - 복사본/01/js/script.js
+++ 02-GNB - 복사본/01/js/script.js
@@ -1,6 +1,7 @@
+//DOM구조가 파악되면 실행
 $(function () {
+  // alert('헤헷');
   // 대상을 변수에 저장
-  const $header = $('header');
   const $menu = $('.gnb>li');
   const $submenu = $('.submenu');
   const duration = 300;
@@ -13,13 +14,10 @@ $(function () {
 
     // 활성화 ㅗ딘 메뉴 표시:on 클래스 부여
     $(this).addClass('on');
-    // header에 active클래스부여
-    $header.addClass('active');
   });
   //메뉴 영역에 마우스가 나가면
   $menu.on('mouseleave', function () {
     $submenu.slideUp(duration);
     $menu.removeClass('on');
-    $header.removeClass('active');
   });
 });
